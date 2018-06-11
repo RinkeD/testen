fc7d3d035dd5ecc632a861941d336997aea89899
Coding Class <rinkedoeser1@gmail.com>
2018-06-11 22:30:37 +0200

    hoi

---
+++ src/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  <h1> Ik neem lekker koffie, ookal moet ik morgen werken heel vroeg </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
